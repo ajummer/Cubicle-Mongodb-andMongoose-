4d24caabbc4af86af9694905bf871b4d3eb623f2
ajummer <makeudreamempty@gmail.com>
2023-09-30 10:50:53 +0300

    connect app to the database

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const handlebarsConfig = require("./config/handlebarsConfig.js");
 const expressConfig = require("./config/expressConfig.js");
 const { port } = require("./constans.js");
 const routes = require("./router.js");
+const dbConnect = require("./config/dbConfig.js");
 
 // local variables
 const app = express();
@@ -10,6 +11,11 @@ const app = express();
 // app configs
 handlebarsConfig(app);
 expressConfig(app);
+
+// Connecting to DB
+dbConnect()
+  .then(() => console.log("Sucessfully connected to the database"))
+  .catch((err) => console.log(err));
 
 // Routing
 app.use(routes);
